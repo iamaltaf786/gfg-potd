b653f37e0a818ede2f401c8b177eb3e903cd17e6
Altaf Hussain <76874013+iamaltaf786@users.noreply.github.com>
2023-07-07 11:08:09 +0530

    no. of 1 bits

---
+++ number-of-1-bits.cpp
@@ -0,0 +1,39 @@
+//{ Driver Code Starts
+#include <bits/stdc++.h>
+using namespace std;
+
+// } Driver Code Ends
+class Solution {
+  public:
+    int setBits(int N) {
+        // Write Your Code here
+        int cn=0;
+        while(N > 0)
+        {
+            if(N%2 == 1)
+            {
+                cn++;
+            }
+            N/=2;
+        }
+        
+        return cn;
+    }
+};
+
+//{ Driver Code Starts.
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int N;
+        cin >> N;
+
+        Solution ob;
+        int cnt = ob.setBits(N);
+        cout << cnt << endl;
+    }
+    return 0;
+}
+
+// } Driver Code Ends
